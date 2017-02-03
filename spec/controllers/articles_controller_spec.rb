fd31cddc9430d54547f2865064bf82bdc00447ff
Vijayendran <vijayendran91@gmail.com>
2017-02-03 11:48:22 -0500

    Modified the Guard file to check routes, controllers, model and views

---
+++ spec/controllers/articles_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe ArticlesController, :type => :controller do
+
+  describe "GET index" do
+    it "returns http success" do
+      get :index
+      expect(response).to_have_http_status(:success)
+    end
+  end
+end
